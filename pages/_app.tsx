53e2c69c24938505a9971b41d16bafab9e82f372
KYEONGMIN CHO <ggingmin@gmail.com>
2023-12-14 18:48:30 +0900

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+
+import 'katex/dist/katex.min.css'
+import 'styles/prism-theme.css'
+import 'prismjs/themes/prism-coy.css'
+import 'react-notion-x/src/styles.css'
+import 'styles/notion.css'
+import 'styles/global.css'
+
+
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
